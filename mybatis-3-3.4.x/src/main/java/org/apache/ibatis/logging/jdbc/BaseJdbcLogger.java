7dfafa7bf20c8393210baf11bae6c4ec59789e12
taotao313 <1913475244@qq.com>
2021-02-18 16:53:00 +0800

    U:日志组件

--- mybatis-3-3.4.x/src/main/java/org/apache/ibatis/logging/jdbc/BaseJdbcLogger.java
+++ mybatis-3-3.4.x/src/main/java/org/apache/ibatis/logging/jdbc/BaseJdbcLogger.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.logging.jdbc;
 
@@ -31,151 +31,171 @@ import org.apache.ibatis.reflection.ArrayUtil;
 
 /**
  * Base class for proxies to do logging
- * 
+ *
  * @author Clinton Begin
  * @author Eduardo Macarron
  */
 public abstract class BaseJdbcLogger {
+    /**
+     * 用于记录PreparedStatement接口中定义的常用的setX()方法
+     */
+    protected static final Set<String> SET_METHODS = new HashSet<String>();
+    /**
+     * 用于记录Statement接口和PreparedStatement接口中执行SQL语句相关的方法
+     */
+    protected static final Set<String> EXECUTE_METHODS = new HashSet<String>();
+    /**
+     * 记录了PreparedStatement.set()方法设置的键值对
+     */
+    private final Map<Object, Object> columnMap = new HashMap<Object, Object>();
+    /**
+     * 记录了PreparedStatement.set()方法设置的key值
+     */
+    private final List<Object> columnNames = new ArrayList<Object>();
+    /**
+     * 记录了PreparedStatement.set()方法设置的value值
+     */
+    private final List<Object> columnValues = new ArrayList<Object>();
+    /**
+     * 用于输出日志的log对象
+     */
+    protected Log statementLog;
+    /**
+     * 记录了SQL的层数，用于格式化输出SQL
+     */
+    protected int queryStack;
 
-  protected static final Set<String> SET_METHODS = new HashSet<String>();
-  protected static final Set<String> EXECUTE_METHODS = new HashSet<String>();
-
-  private final Map<Object, Object> columnMap = new HashMap<Object, Object>();
-
-  private final List<Object> columnNames = new ArrayList<Object>();
-  private final List<Object> columnValues = new ArrayList<Object>();
-
-  protected Log statementLog;
-  protected int queryStack;
-
-  /*
-   * Default constructor
-   */
-  public BaseJdbcLogger(Log log, int queryStack) {
-    this.statementLog = log;
-    if (queryStack == 0) {
-      this.queryStack = 1;
-    } else {
-      this.queryStack = queryStack;
+    /**
+     * 构造器
+     *
+     * @param log
+     * @param queryStack
+     */
+    public BaseJdbcLogger(Log log, int queryStack) {
+        this.statementLog = log;
+        if (queryStack == 0) {
+            this.queryStack = 1;
+        } else {
+            this.queryStack = queryStack;
+        }
     }
-  }
 
-  static {
-    SET_METHODS.add("setString");
-    SET_METHODS.add("setNString");
-    SET_METHODS.add("setInt");
-    SET_METHODS.add("setByte");
-    SET_METHODS.add("setShort");
-    SET_METHODS.add("setLong");
-    SET_METHODS.add("setDouble");
-    SET_METHODS.add("setFloat");
-    SET_METHODS.add("setTimestamp");
-    SET_METHODS.add("setDate");
-    SET_METHODS.add("setTime");
-    SET_METHODS.add("setArray");
-    SET_METHODS.add("setBigDecimal");
-    SET_METHODS.add("setAsciiStream");
-    SET_METHODS.add("setBinaryStream");
-    SET_METHODS.add("setBlob");
-    SET_METHODS.add("setBoolean");
-    SET_METHODS.add("setBytes");
-    SET_METHODS.add("setCharacterStream");
-    SET_METHODS.add("setNCharacterStream");
-    SET_METHODS.add("setClob");
-    SET_METHODS.add("setNClob");
-    SET_METHODS.add("setObject");
-    SET_METHODS.add("setNull");
+    static {
+        SET_METHODS.add("setString");
+        SET_METHODS.add("setNString");
+        SET_METHODS.add("setInt");
+        SET_METHODS.add("setByte");
+        SET_METHODS.add("setShort");
+        SET_METHODS.add("setLong");
+        SET_METHODS.add("setDouble");
+        SET_METHODS.add("setFloat");
+        SET_METHODS.add("setTimestamp");
+        SET_METHODS.add("setDate");
+        SET_METHODS.add("setTime");
+        SET_METHODS.add("setArray");
+        SET_METHODS.add("setBigDecimal");
+        SET_METHODS.add("setAsciiStream");
+        SET_METHODS.add("setBinaryStream");
+        SET_METHODS.add("setBlob");
+        SET_METHODS.add("setBoolean");
+        SET_METHODS.add("setBytes");
+        SET_METHODS.add("setCharacterStream");
+        SET_METHODS.add("setNCharacterStream");
+        SET_METHODS.add("setClob");
+        SET_METHODS.add("setNClob");
+        SET_METHODS.add("setObject");
+        SET_METHODS.add("setNull");
 
-    EXECUTE_METHODS.add("execute");
-    EXECUTE_METHODS.add("executeUpdate");
-    EXECUTE_METHODS.add("executeQuery");
-    EXECUTE_METHODS.add("addBatch");
-  }
-
-  protected void setColumn(Object key, Object value) {
-    columnMap.put(key, value);
-    columnNames.add(key);
-    columnValues.add(value);
-  }
-
-  protected Object getColumn(Object key) {
-    return columnMap.get(key);
-  }
-
-  protected String getParameterValueString() {
-    List<Object> typeList = new ArrayList<Object>(columnValues.size());
-    for (Object value : columnValues) {
-      if (value == null) {
-        typeList.add("null");
-      } else {
-        typeList.add(objectValueString(value) + "(" + value.getClass().getSimpleName() + ")");
-      }
+        EXECUTE_METHODS.add("execute");
+        EXECUTE_METHODS.add("executeUpdate");
+        EXECUTE_METHODS.add("executeQuery");
+        EXECUTE_METHODS.add("addBatch");
     }
-    final String parameters = typeList.toString();
-    return parameters.substring(1, parameters.length() - 1);
-  }
 
-  protected String objectValueString(Object value) {
-    if (value instanceof Array) {
-      try {
-        return ArrayUtil.toString(((Array) value).getArray());
-      } catch (SQLException e) {
+    protected void setColumn(Object key, Object value) {
+        columnMap.put(key, value);
+        columnNames.add(key);
+        columnValues.add(value);
+    }
+
+    protected Object getColumn(Object key) {
+        return columnMap.get(key);
+    }
+
+    protected String getParameterValueString() {
+        List<Object> typeList = new ArrayList<Object>(columnValues.size());
+        for (Object value : columnValues) {
+            if (value == null) {
+                typeList.add("null");
+            } else {
+                typeList.add(objectValueString(value) + "(" + value.getClass().getSimpleName() + ")");
+            }
+        }
+        final String parameters = typeList.toString();
+        return parameters.substring(1, parameters.length() - 1);
+    }
+
+    protected String objectValueString(Object value) {
+        if (value instanceof Array) {
+            try {
+                return ArrayUtil.toString(((Array) value).getArray());
+            } catch (SQLException e) {
+                return value.toString();
+            }
+        }
         return value.toString();
-      }
     }
-    return value.toString();
-  }
 
-  protected String getColumnString() {
-    return columnNames.toString();
-  }
-
-  protected void clearColumnInfo() {
-    columnMap.clear();
-    columnNames.clear();
-    columnValues.clear();
-  }
-
-  protected String removeBreakingWhitespace(String original) {
-    StringTokenizer whitespaceStripper = new StringTokenizer(original);
-    StringBuilder builder = new StringBuilder();
-    while (whitespaceStripper.hasMoreTokens()) {
-      builder.append(whitespaceStripper.nextToken());
-      builder.append(" ");
+    protected String getColumnString() {
+        return columnNames.toString();
     }
-    return builder.toString();
-  }
 
-  protected boolean isDebugEnabled() {
-    return statementLog.isDebugEnabled();
-  }
-
-  protected boolean isTraceEnabled() {
-    return statementLog.isTraceEnabled();
-  }
-
-  protected void debug(String text, boolean input) {
-    if (statementLog.isDebugEnabled()) {
-      statementLog.debug(prefix(input) + text);
+    protected void clearColumnInfo() {
+        columnMap.clear();
+        columnNames.clear();
+        columnValues.clear();
     }
-  }
 
-  protected void trace(String text, boolean input) {
-    if (statementLog.isTraceEnabled()) {
-      statementLog.trace(prefix(input) + text);
+    protected String removeBreakingWhitespace(String original) {
+        StringTokenizer whitespaceStripper = new StringTokenizer(original);
+        StringBuilder builder = new StringBuilder();
+        while (whitespaceStripper.hasMoreTokens()) {
+            builder.append(whitespaceStripper.nextToken());
+            builder.append(" ");
+        }
+        return builder.toString();
     }
-  }
 
-  private String prefix(boolean isInput) {
-    char[] buffer = new char[queryStack * 2 + 2];
-    Arrays.fill(buffer, '=');
-    buffer[queryStack * 2 + 1] = ' ';
-    if (isInput) {
-      buffer[queryStack * 2] = '>';
-    } else {
-      buffer[0] = '<';
+    protected boolean isDebugEnabled() {
+        return statementLog.isDebugEnabled();
     }
-    return new String(buffer);
-  }
+
+    protected boolean isTraceEnabled() {
+        return statementLog.isTraceEnabled();
+    }
+
+    protected void debug(String text, boolean input) {
+        if (statementLog.isDebugEnabled()) {
+            statementLog.debug(prefix(input) + text);
+        }
+    }
+
+    protected void trace(String text, boolean input) {
+        if (statementLog.isTraceEnabled()) {
+            statementLog.trace(prefix(input) + text);
+        }
+    }
+
+    private String prefix(boolean isInput) {
+        char[] buffer = new char[queryStack * 2 + 2];
+        Arrays.fill(buffer, '=');
+        buffer[queryStack * 2 + 1] = ' ';
+        if (isInput) {
+            buffer[queryStack * 2] = '>';
+        } else {
+            buffer[0] = '<';
+        }
+        return new String(buffer);
+    }
 
 }
--- mybatis-3-3.4.x/src/main/java/org/apache/ibatis/logging/jdbc/ResultSetLogger.java
+++ mybatis-3-3.4.x/src/main/java/org/apache/ibatis/logging/jdbc/ResultSetLogger.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.logging.jdbc;
 
@@ -30,123 +30,137 @@ import org.apache.ibatis.reflection.ExceptionUtil;
 
 /**
  * ResultSet proxy to add logging
- * 
+ *
  * @author Clinton Begin
  * @author Eduardo Macarron
- * 
+ *
  */
 public final class ResultSetLogger extends BaseJdbcLogger implements InvocationHandler {
+    /**
+     * 记录了超大长度的类型
+     */
+    private static Set<Integer> BLOB_TYPES = new HashSet<Integer>();
+    /**
+     * 是否是ResultSet结果集的第一行
+     */
+    private boolean first = true;
+    /**
+     * 统计行数
+     */
+    private int rows;
+    /**
+     * 真是的ResuleSet对象
+     */
+    private final ResultSet rs;
+    /**
+     * 记录了超大字段的列编号
+     */
+    private final Set<Integer> blobColumns = new HashSet<Integer>();
 
-  private static Set<Integer> BLOB_TYPES = new HashSet<Integer>();
-  private boolean first = true;
-  private int rows;
-  private final ResultSet rs;
-  private final Set<Integer> blobColumns = new HashSet<Integer>();
+    static {
+        BLOB_TYPES.add(Types.BINARY);
+        BLOB_TYPES.add(Types.BLOB);
+        BLOB_TYPES.add(Types.CLOB);
+        BLOB_TYPES.add(Types.LONGNVARCHAR);
+        BLOB_TYPES.add(Types.LONGVARBINARY);
+        BLOB_TYPES.add(Types.LONGVARCHAR);
+        BLOB_TYPES.add(Types.NCLOB);
+        BLOB_TYPES.add(Types.VARBINARY);
+    }
 
-  static {
-    BLOB_TYPES.add(Types.BINARY);
-    BLOB_TYPES.add(Types.BLOB);
-    BLOB_TYPES.add(Types.CLOB);
-    BLOB_TYPES.add(Types.LONGNVARCHAR);
-    BLOB_TYPES.add(Types.LONGVARBINARY);
-    BLOB_TYPES.add(Types.LONGVARCHAR);
-    BLOB_TYPES.add(Types.NCLOB);
-    BLOB_TYPES.add(Types.VARBINARY);
-  }
-  
-  private ResultSetLogger(ResultSet rs, Log statementLog, int queryStack) {
-    super(statementLog, queryStack);
-    this.rs = rs;
-  }
+    private ResultSetLogger(ResultSet rs, Log statementLog, int queryStack) {
+        super(statementLog, queryStack);
+        this.rs = rs;
+    }
 
-  @Override
-  public Object invoke(Object proxy, Method method, Object[] params) throws Throwable {
-    try {
-      if (Object.class.equals(method.getDeclaringClass())) {
-        return method.invoke(this, params);
-      }    
-      Object o = method.invoke(rs, params);
-      if ("next".equals(method.getName())) {
-        if (((Boolean) o)) {
-          rows++;
-          if (isTraceEnabled()) {
-            ResultSetMetaData rsmd = rs.getMetaData();
-            final int columnCount = rsmd.getColumnCount();
-            if (first) {
-              first = false;
-              printColumnHeaders(rsmd, columnCount);
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] params) throws Throwable {
+        try {
+            if (Object.class.equals(method.getDeclaringClass())) {
+                return method.invoke(this, params);
             }
-            printColumnValues(columnCount);
-          }
-        } else {
-          debug("     Total: " + rows, false);
+            Object o = method.invoke(rs, params);
+            if ("next".equals(method.getName())) {
+                if (((Boolean) o)) {
+                    rows++;
+                    if (isTraceEnabled()) {
+                        ResultSetMetaData rsmd = rs.getMetaData();
+                        final int columnCount = rsmd.getColumnCount();
+                        if (first) {
+                            first = false;
+                            printColumnHeaders(rsmd, columnCount);
+                        }
+                        printColumnValues(columnCount);
+                    }
+                } else {
+                    debug("     Total: " + rows, false);
+                }
+            }
+            clearColumnInfo();
+            return o;
+        } catch (Throwable t) {
+            throw ExceptionUtil.unwrapThrowable(t);
         }
-      }
-      clearColumnInfo();
-      return o;
-    } catch (Throwable t) {
-      throw ExceptionUtil.unwrapThrowable(t);
     }
-  }
 
-  private void printColumnHeaders(ResultSetMetaData rsmd, int columnCount) throws SQLException {
-    StringBuilder row = new StringBuilder();
-    row.append("   Columns: ");
-    for (int i = 1; i <= columnCount; i++) {
-      if (BLOB_TYPES.contains(rsmd.getColumnType(i))) {
-        blobColumns.add(i);
-      }
-      String colname = rsmd.getColumnLabel(i);
-      row.append(colname);
-      if (i != columnCount) {
-        row.append(", ");
-      }
-    }
-    trace(row.toString(), false);
-  }
-
-  private void printColumnValues(int columnCount) {
-    StringBuilder row = new StringBuilder();
-    row.append("       Row: ");
-    for (int i = 1; i <= columnCount; i++) {
-      String colname;
-      try {
-        if (blobColumns.contains(i)) {
-          colname = "<<BLOB>>";
-        } else {
-          colname = rs.getString(i);
+    private void printColumnHeaders(ResultSetMetaData rsmd, int columnCount) throws SQLException {
+        StringBuilder row = new StringBuilder();
+        row.append("   Columns: ");
+        for (int i = 1; i <= columnCount; i++) {
+            if (BLOB_TYPES.contains(rsmd.getColumnType(i))) {
+                blobColumns.add(i);
+            }
+            String colname = rsmd.getColumnLabel(i);
+            row.append(colname);
+            if (i != columnCount) {
+                row.append(", ");
+            }
         }
-      } catch (SQLException e) {
-        // generally can't call getString() on a BLOB column
-        colname = "<<Cannot Display>>";
-      }
-      row.append(colname);
-      if (i != columnCount) {
-        row.append(", ");
-      }
+        trace(row.toString(), false);
     }
-    trace(row.toString(), false);
-  }
 
-  /*
-   * Creates a logging version of a ResultSet
-   *
-   * @param rs - the ResultSet to proxy
-   * @return - the ResultSet with logging
-   */
-  public static ResultSet newInstance(ResultSet rs, Log statementLog, int queryStack) {
-    InvocationHandler handler = new ResultSetLogger(rs, statementLog, queryStack);
-    ClassLoader cl = ResultSet.class.getClassLoader();
-    return (ResultSet) Proxy.newProxyInstance(cl, new Class[]{ResultSet.class}, handler);
-  }
+    private void printColumnValues(int columnCount) {
+        StringBuilder row = new StringBuilder();
+        row.append("       Row: ");
+        for (int i = 1; i <= columnCount; i++) {
+            String colname;
+            try {
+                if (blobColumns.contains(i)) {
+                    colname = "<<BLOB>>";
+                } else {
+                    colname = rs.getString(i);
+                }
+            } catch (SQLException e) {
+                // generally can't call getString() on a BLOB column
+                colname = "<<Cannot Display>>";
+            }
+            row.append(colname);
+            if (i != columnCount) {
+                row.append(", ");
+            }
+        }
+        trace(row.toString(), false);
+    }
 
-  /*
-   * Get the wrapped result set
-   *
-   * @return the resultSet
-   */
-  public ResultSet getRs() {
-    return rs;
-  }
+    /*
+     * Creates a logging version of a ResultSet
+     *
+     * @param rs - the ResultSet to proxy
+     * @return - the ResultSet with logging
+     */
+    public static ResultSet newInstance(ResultSet rs, Log statementLog, int queryStack) {
+        InvocationHandler handler = new ResultSetLogger(rs, statementLog, queryStack);
+        ClassLoader cl = ResultSet.class.getClassLoader();
+        return (ResultSet) Proxy.newProxyInstance(cl, new Class[]{ResultSet.class}, handler);
+    }
+
+    /*
+     * Get the wrapped result set
+     *
+     * @return the resultSet
+     */
+    public ResultSet getRs() {
+        return rs;
+    }
 
 }
