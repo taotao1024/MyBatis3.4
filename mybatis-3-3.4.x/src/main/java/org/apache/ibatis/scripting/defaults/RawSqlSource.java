32635597366caf17cc9df4bbb903e9ab6a430356
taotao313 <1913475244@qq.com>
2021-02-23 14:18:19 +0800

    U:SQL解析相关+脚本

--- mybatis-3-3.4.x/src/main/java/org/apache/ibatis/scripting/defaults/RawSqlSource.java
+++ mybatis-3-3.4.x/src/main/java/org/apache/ibatis/scripting/defaults/RawSqlSource.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.scripting.defaults;
 
@@ -26,35 +26,43 @@ import org.apache.ibatis.scripting.xmltags.SqlNode;
 import org.apache.ibatis.session.Configuration;
 
 /**
- * Static SqlSource. It is faster than {@link DynamicSqlSource} because mappings are 
+ * 模式：组合模式
+ * <p>
+ * 角色：
+ * <p>
+ * 处理静态语句
+ * <p>
+ * 静态SQL可以直接提交给数据库执行，SQL中可能会含有"?"占位符
+ * <p>
+ * Static SqlSource. It is faster than {@link DynamicSqlSource} because mappings are
  * calculated during startup.
- * 
- * @since 3.2.0
+ *
  * @author Eduardo Macarron
+ * @since 3.2.0
  */
 public class RawSqlSource implements SqlSource {
 
-  private final SqlSource sqlSource;
+    private final SqlSource sqlSource;
 
-  public RawSqlSource(Configuration configuration, SqlNode rootSqlNode, Class<?> parameterType) {
-    this(configuration, getSql(configuration, rootSqlNode), parameterType);
-  }
+    public RawSqlSource(Configuration configuration, SqlNode rootSqlNode, Class<?> parameterType) {
+        this(configuration, getSql(configuration, rootSqlNode), parameterType);
+    }
 
-  public RawSqlSource(Configuration configuration, String sql, Class<?> parameterType) {
-    SqlSourceBuilder sqlSourceParser = new SqlSourceBuilder(configuration);
-    Class<?> clazz = parameterType == null ? Object.class : parameterType;
-    sqlSource = sqlSourceParser.parse(sql, clazz, new HashMap<String, Object>());
-  }
+    public RawSqlSource(Configuration configuration, String sql, Class<?> parameterType) {
+        SqlSourceBuilder sqlSourceParser = new SqlSourceBuilder(configuration);
+        Class<?> clazz = parameterType == null ? Object.class : parameterType;
+        sqlSource = sqlSourceParser.parse(sql, clazz, new HashMap<String, Object>());
+    }
 
-  private static String getSql(Configuration configuration, SqlNode rootSqlNode) {
-    DynamicContext context = new DynamicContext(configuration, null);
-    rootSqlNode.apply(context);
-    return context.getSql();
-  }
+    private static String getSql(Configuration configuration, SqlNode rootSqlNode) {
+        DynamicContext context = new DynamicContext(configuration, null);
+        rootSqlNode.apply(context);
+        return context.getSql();
+    }
 
-  @Override
-  public BoundSql getBoundSql(Object parameterObject) {
-    return sqlSource.getBoundSql(parameterObject);
-  }
+    @Override
+    public BoundSql getBoundSql(Object parameterObject) {
+        return sqlSource.getBoundSql(parameterObject);
+    }
 
 }
--- mybatis-3-3.4.x/src/main/java/org/apache/ibatis/scripting/xmltags/OgnlCache.java
+++ mybatis-3-3.4.x/src/main/java/org/apache/ibatis/scripting/xmltags/OgnlCache.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.scripting.xmltags;
 
@@ -24,36 +24,43 @@ import ognl.OgnlException;
 import org.apache.ibatis.builder.BuilderException;
 
 /**
- * Caches OGNL parsed expressions.
+ * OgnlCache对原生的OGNL进行了封装，因为OGNL解析过程比较耗时，为了提高效率，使用expressionCache字段对解析后的OGNL表达式进行缓存
  *
  * @author Eduardo Macarron
- *
  * @see <a href='http://code.google.com/p/mybatis/issues/detail?id=342'>Issue 342</a>
  */
 public final class OgnlCache {
+    /**
+     * 静态成员变量
+     */
+    private static final Map<String, Object> expressionCache = new ConcurrentHashMap<String, Object>();
 
-  private static final Map<String, Object> expressionCache = new ConcurrentHashMap<String, Object>();
-
-  private OgnlCache() {
-    // Prevent Instantiation of Static Class
-  }
-
-  public static Object getValue(String expression, Object root) {
-    try {
-      Map<Object, OgnlClassResolver> context = Ognl.createDefaultContext(root, new OgnlClassResolver());
-      return Ognl.getValue(parseExpression(expression), context, root);
-    } catch (OgnlException e) {
-      throw new BuilderException("Error evaluating expression '" + expression + "'. Cause: " + e, e);
+    private OgnlCache() {
+        // Prevent Instantiation of Static Class
     }
-  }
 
-  private static Object parseExpression(String expression) throws OgnlException {
-    Object node = expressionCache.get(expression);
-    if (node == null) {
-      node = Ognl.parseExpression(expression);
-      expressionCache.put(expression, node);
+    public static Object getValue(String expression, Object root) {
+        try {
+            // 创建OgnlContext对象，OgnlClassResolver替代了OGNL中原有的DefaultClassResolver
+            // 主要功能是使用前面介绍的Resource工具类定位资源
+            Map<Object, OgnlClassResolver> context = Ognl.createDefaultContext(root, new OgnlClassResolver());
+            // 使用OGNL执行expression表达式
+            return Ognl.getValue(parseExpression(expression), context, root);
+        } catch (OgnlException e) {
+            throw new BuilderException("Error evaluating expression '" + expression + "'. Cause: " + e, e);
+        }
     }
-    return node;
-  }
+
+    private static Object parseExpression(String expression) throws OgnlException {
+        // 查找缓存
+        Object node = expressionCache.get(expression);
+        if (node == null) {
+            // 解析表达式
+            node = Ognl.parseExpression(expression);
+            // 将表达式的解析结果创建到缓存中
+            expressionCache.put(expression, node);
+        }
+        return node;
+    }
 
 }
